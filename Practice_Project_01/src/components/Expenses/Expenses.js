90d276ca61f5726e05b11c27c687f9799a29affc
L-Lawliet27 <andressalazarcolina@gmail>
2023-02-07 20:44:52 +0100

    Practice1

---
+++ Practice_Project_01/src/components/Expenses/Expenses.js
@@ -0,0 +1,31 @@
+import "./Expenses.css";
+import ExpenseFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import { useState } from "react";
+import Card from "../UI/Card";
+
+function Expenses(props) {
+  const [enteredYear, setYear] = useState("2020");
+
+  const yearChangeHandler = (selectedYear) => {
+    setYear(selectedYear);
+  };
+
+  const filteredExpenses = props.expenseItems.filter((expense) => {
+    return expense.date.getFullYear().toString() === enteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={enteredYear}
+          onSelectedYear={yearChangeHandler}
+        />
+        <ExpensesList items={filteredExpenses}/>
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
